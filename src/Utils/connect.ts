600731da4dc1d7e7420a30b0b92bfbe6f8b4d7c4
Rohit Kumar <rohitkumar@curioventures.com>
2023-01-21 18:56:50 +0530

    .

--- src/Utils/connect.ts
+++ src/Utils/connect.ts
@@ -1,9 +1,9 @@
 import mongoose from "mongoose";
 
 async function connect() {
-  const dbUri: string = "mongodb://localhost:27017/car_counter";
-  // const dbUri: string =
-  //   "mongodb+srv://razerspeed:Pulsar200ns@cluster0.pgoe7mv.mongodb.net/test";
+  // const dbUri: string = "mongodb://localhost:27017/car_counter";
+  const dbUri: string =
+    "mongodb+srv://razerspeed:Pulsar200ns@cluster0.pgoe7mv.mongodb.net/test";
 
   try {
     mongoose.set("strictQuery", true);
